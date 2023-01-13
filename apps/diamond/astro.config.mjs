ded1d811525eb0f039492800eb562525bc08b71f
NathaelB <kirua48000@gmail.com>
2023-01-13 18:06:26 +0100

    :init: Initialization of the workspace `leadocde.fr`

--- apps/diamond/astro.config.mjs
+++ apps/diamond/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+
+// https://astro.build/config
 export default defineConfig({
-  outDir: '../../dist/apps/diamond',
+    outDir: '../../dist/apps/diamond',
+    integrations: []
 });
